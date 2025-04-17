bf1e4426058c8fd7e2a3d78c8cd21a88344050c5
Rodrigo Garcia <litoralwastaken@gmail.com>
2025-04-17 11:26:44 +0200

    added icons

--- src/main.js
+++ src/main.js
@@ -12,9 +12,15 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faCopyright } from '@fortawesome/free-regular-svg-icons'
 
 /* add icons to the library */
 library.add(faUserSecret)
 library.add(faBars)
+library.add(faChevronDown)
+library.add(faChevronUp)
+library.add(faCopyright)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
